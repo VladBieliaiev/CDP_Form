88118b0b73561661aa50e694255e9b5c975b675b
VladBieliaiev <vlaaab9@gmail.com>
2024-02-07 11:56:33 +0100

    add fucntion to change btn text

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function toggleForm(removeClass, addClass) {
+function toggleForm(removeClass, addClass, text) {
+  
   const form = document.getElementById("form");
   form.classList.remove(removeClass);
   form.classList.add(addClass);
+
+  const submitBtn = document.getElementById("submitBtn");
+  submitBtn.innerText = text;
 }
